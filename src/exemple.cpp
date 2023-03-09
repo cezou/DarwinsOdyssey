ef49f34e01041cba25336a51092cfbbadf9c3fff
jofrix98 <jofre.coll10@gmail.com>
2023-03-09 12:05:27 +0100

    commit 3

--- src/exemple.cpp
+++ src/exemple.cpp
@@ -1,48 +1,41 @@
 #define OLC_PGE_APPLICATION
 #include "../inc/olcPixelGameEngine-master/olcPixelGameEngine.h"
-#include "./Player.h"
+//#include "./Player.h"
+#include "./Map.h"
 
-class Example : public olc::PixelGameEngine
+class Game : public olc::PixelGameEngine
 {
-public:
-	Example()
-	{
-		sAppName = "Darwin's Odyssey";
-	}
+	public:
+		game()
+		{
+			sAppName = "Darwin's Odyssey";
+		}
 
-public:
-	bool OnUserCreate() override
-	{
-		// Called once at the start, so create things here
-		return true;
-	}
+		Map mapLevel1;
+		
+	public:
 
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		// called once per frame
-		for (int x = 0; x <= ScreenWidth()/2; x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(0, 0, 255));	
-	
-	Player P;			
-	DrawCircle(P.x, P.y, 10, olc::Pixel(255, 255, 255));
-	P.move();
 
-		for (int x = ScreenWidth()/2; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(0, 255, 0 ));	
-				
-		return true;
-	}
+		bool OnUserCreate() override
+		{
+			// Called once at the start, so create things here
+			return true;
+		}
+
+		bool OnUserUpdate(float fElapsedTime) override
+		{
+
+
+		}
 };
 
 
 int main()
 {
-	Example player1;
+	Game game;
 
-	if (player1.Construct(240, 135, 8, 8))
-		player1.Start();
+	if (game.Construct(240, 135, 8, 8))
+		game.Start();
 		
 
 	return 0;
